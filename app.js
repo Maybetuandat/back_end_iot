f6742cd635666f92e7fa4e2c4d895770665fdeda
Maybetuandat <trinhvinhtuandat05102003@gmail.com>
2024-10-01 14:37:11 +0700

    first commit

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ apiRouter(app);
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 
-app.listen(3000, () => {
+app.listen(3000, process.env.SERVER_HOST, () => {
   console.log("Server is running on port 3000");
 });
 server.listen( 9999, process.env.SERVER_HOST, () => {
